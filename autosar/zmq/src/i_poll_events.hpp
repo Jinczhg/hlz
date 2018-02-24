ea7e3eeb494380efe1aaa78d1f9f759e94a9b2a1
朱光育 <guangyu.zhu@hinge-tech.com>
2018-02-24 17:20:02 +0800

    update

--- autosar/zmq/src/i_poll_events.hpp
+++ autosar/zmq/src/i_poll_events.hpp
@@ -41,13 +41,13 @@ namespace zmq
         virtual ~i_poll_events () {}
 
         // Called by I/O thread when file descriptor is ready for reading.
-        virtual void in_event () = 0;
+        virtual void in_event () {}
 
         // Called by I/O thread when file descriptor is ready for writing.
-        virtual void out_event () = 0;
+        virtual void out_event () {}
 
         // Called when timer expires.
-        virtual void timer_event (int id_) = 0;
+        virtual void timer_event (int id_) {}
     };
 
 }
